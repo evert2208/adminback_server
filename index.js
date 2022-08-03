726117aa2b7c3ec20d323b7bb0e84690e5bfdc7b
evert2208 <65311634+evert2208@users.noreply.github.com>
2022-08-03 15:42:04 -0500

    ultimo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 var cors = require('cors');
 const { dbConnection } = require('./database/config');
+const path = require('path');
 
 
 //Crear servidor de express
@@ -33,6 +34,10 @@ app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/login', require('./routes/auth'));
 app.use('/api/upload', require('./routes/uploads'));
 
+//ultimo
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen(process.env.PORT, () => {
     console.log('servidor corriendo puerto ' + process.env.PORT);
